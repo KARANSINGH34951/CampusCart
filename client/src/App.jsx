14cba3040db413f5710dab388b5eef06eee850c1
karan singh D <ceitkaransingh2026@gmail.com>
2024-11-12 19:39:34 +0530

    Added Routes Logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,16 @@ import AuthLayout from './components/auth/AuthLayout'
 import Register from "./pages/auth/Register"
 import Login from "./pages/auth/Login"
 import AdminLayout from './components/admin-view/Layout'
+import AdminDashboard from './pages/admin-view/AdminDashboard'
+import AdminProducts from './pages/admin-view/AdminProducts'
+import AdminOrders from './pages/admin-view/AdminOrders'
+import AdminFeatures from './pages/admin-view/AdminFeatures'
+import ShoppingLayout from './components/shopping-view/ShoppingLayout'
+import NotFound from './pages/NotFound/NotFound'
+import Home from "./pages/shopping-view/Home"
+import Listing from './pages/shopping-view/Listing'
+import Checkout from './pages/shopping-view/Checkout'
+import Account from './pages/shopping-view/Account'
 
 function App() {
  
@@ -11,16 +21,28 @@ function App() {
 <>
    <Router>
     <Routes>
+      
       <Route path='/' element={<AuthLayout />} >
-        <Route path='register' element={<Register/>} />
+        <Route index element={<Register/>} />
         <Route path='login' element={<Login/>}/>
       </Route>
 
       <Route path='/admin' element={<AdminLayout/>}>
-        <Route path='dashboard' element={<div>Dashboard</div>}/>
-        <Route path='products' element={<div>Products</div>}/>
-        <Route path='orders' element={<div>Orders</div>}/>
+        <Route path='dashboard' element={<AdminDashboard/>}/>
+        <Route path='products' element={<AdminProducts/>}/>
+        <Route path='orders' element={<AdminOrders/>}/>
+        <Route path='feature' element={<AdminFeatures/>}/>
       </Route>
+
+      <Route path='/shop' element={<ShoppingLayout/>}>
+        <Route path='home' element={<Home/>}/>
+        <Route path='listing' element={<Listing/>}/>
+        <Route path='checkout' element={<Checkout/>}/>
+        <Route path='account' element={<Account/>}/>
+      </Route>
+
+      <Route path='*' element={<NotFound/>}/>
+      
     </Routes>
    </Router>
 </>
---
+++ client/src/pages/shopping-view/Listing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Listting = () => {
+  return (
+    <div>Listting</div>
+  )
+}
+
+export default Listting
