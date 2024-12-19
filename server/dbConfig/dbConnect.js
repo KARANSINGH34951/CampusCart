b323008765bad76f95cd1d382cb4bdeb0598a240
karan singh D <ceitkaransingh2026@gmail.com>
2024-12-19 18:28:17 +0530

    Data base connection

--- server/dbConfig/dbConnect.js
+++ server/dbConfig/dbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const dbConnect=async ()=> {
- await  mongoose.connect('mongodb://127.0.0.1:27017/campuscart')
+ await  mongoose.connect(process.env.MONGO_URI)
 }
